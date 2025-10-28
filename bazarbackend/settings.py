5c4025b3e2b7f603d0df04cf08e644fafbe8dab3
rahu12-tech <jakhar365365@gmail.com>
2025-10-28 11:02:50 +0530

    add origin

--- bazarbackend/settings.py
+++ bazarbackend/settings.py
@@ -69,6 +69,7 @@ CORS_ALLOW_ALL_ORIGINS = True  # Local testing ke liye ✅
 # 👇 Optional: sirf frontend port allow karna ho
 CORS_ALLOWED_ORIGINS = [
     "https://pixelbazarfrontend.onrender.com",
+    "https://pixelbazar-backend.onrender.com",
     "http://localhost:5173",  # 👈 Vite frontend port
     "http://127.0.0.1:5173",
 ]
